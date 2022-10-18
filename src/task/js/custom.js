ba3c188432eb81283d7ec1097d1e4e9261141d5d
wsyysw <88481703+wsyysw@users.noreply.github.com>
2022-10-18 12:06:13 +1000

    Delete src/task directory

--- src/task/js/custom.js
+++
@@ -1,7 +0,0 @@
-$( document ).ready(function() {
-    console.log( "ready!" );
-    $( ".changeStatus option" ).click(function(e) {
-        console.log(this.value);
-        console.log($(this).value);
-    });
-});
